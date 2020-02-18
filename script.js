781f13c1ac880e367fe505ddb57b93bf92953988
MaryHooker <mc020791@yahoo.com>
2020-02-18 13:28:12 -0600

    Problem 2

--- script.js
+++ script.js
@@ -1,52 +1,58 @@
-// // console.log("Yoohoo Worldoo")
 
-// # JavaScript template strings class work
+// // # JavaScript template strings class work
 
-// ### Exercise 1:
-// Create a template string and assign it to the variable ```my_template_string```.
-// The template string should contain the following data only:
+// // ### Exercise 1:
+// // Create a template string and assign it to the variable ```my_template_string```.
+// // The template string should contain the following data only:
 
-// ```
+// // ```
+// // My name is ${my_name}.
+// // My id is ${my_id}.
+// // My address is ${my_address}.
+// // ```
+// // Create the variables ```my_name```, ```my_id```, and ```my_address``` and set their values to ```Kevin```, ```2112```, and ```Memphis, TN```.
+
+// // Use the template string you created and assigned to the variable ```my_template_string``` to log the formatted output to the console.
+
+// // Sample Output:
+// // ```
+// // My name is Kevin.
+// // My id is 2112.
+// // My address is Memphis, TN.
+// // ```
+// let my_name = "Kevin"
+// let my_id = "2112"
+// let my_address = "Memphis, TN"
+
+// let my_template_string = 
+// `
 // My name is ${my_name}.
 // My id is ${my_id}.
 // My address is ${my_address}.
-// ```
-// Create the variables ```my_name```, ```my_id```, and ```my_address``` and set their values to ```Kevin```, ```2112```, and ```Memphis, TN```.
+// `
 
-// Use the template string you created and assigned to the variable ```my_template_string``` to log the formatted output to the console.
+// console.log(my_template_string)
 
-// Sample Output:
-// ```
-// My name is Kevin.
-// My id is 2112.
-// My address is Memphis, TN.
-// ```
-let my_name = "Kevin"
-let my_id = "2112"
-let my_address = "Memphis, TN"
+// // ### Exercise 2:
 
-let my_template_string = 
-`
-My name is ${my_name}.
-My id is ${my_id}.
-My address is ${my_address}.
-`
+// // Create a new template string 
 
-console.log(my_template_string)
+// // ```
+// // var contestant = "1_M_l337!";
+// // var score = "9001";
+// // ```
 
-// ### Exercise 2:
-
-// Create a new template string 
-
-// ```
-// var contestant = "1_M_l337!";
-// var score = "9001";
-// ```
-
-// and use the provided variables to print 
-// ```
-// Congratulations 1_M_l337! !!, your score is 9001.
-// ```
+// // and use the provided variables to print 
+// // ```
+// // Congratulations 1_M_l337! !!, your score is 9001.
+// // ```
+// let contestant = "1_M_1337"
+// let score = "9001"
+// let new_string =
+// `
+// Congratulations ${contestant}!!!, your score is ${score}.
+// `
+// console.log(new_string)
 
 // ### Exercise 3:
 
