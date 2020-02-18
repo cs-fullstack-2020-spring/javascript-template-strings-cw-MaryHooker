b1fd1f5836f509cf5972479a78f3ae57fa802212
autumn-ragland <raglanda998@gmail.com>
2020-02-18 16:00:42 -0600

    graded

--- script.js
+++ script.js
@@ -20,6 +20,7 @@
 // // My id is 2112.
 // // My address is Memphis, TN.
 // // ```
+
 // //***Declaring variables */
 // let my_name = "Kevin"
 // let my_id = "2112"
@@ -31,7 +32,7 @@
 // My id is ${my_id}.
 // My address is ${my_address}.
 // `
-// //***Printing literal in console */
+//***Printing literal in console */
 // console.log(my_template_string)
 
 // // ### Exercise 2:
@@ -47,6 +48,7 @@
 // // ```
 // // Congratulations 1_M_l337! !!, your score is 9001.
 // // ```
+
 // //****Declaring variables */
 // let contestant = "1_M_1337"
 // let score = "9001"
@@ -58,26 +60,26 @@
 // //***printing literal in console */
 // console.log(new_string)
 
-// // ### Exercise 3:
+// ### Exercise 3:
 
-// // - Prompts the user for the name of their Home team
-// // - Prompts the user for the name of the Visiting team
-// // - Prompt the user to enter the score for their home team *(include the team name when prompting the user)*
-// // `How many points did the Memphis Grizzlies score? (Home team score)`
-// // - Prompt the user to enter the score for the visiting team *(include the team name when prompting the user)*
-// // `How many points did the GS Warriors score? (Visting team score)`
+// - Prompts the user for the name of their Home team
+// - Prompts the user for the name of the Visiting team
+// - Prompt the user to enter the score for their home team *(include the team name when prompting the user)*
+// `How many points did the Memphis Grizzlies score? (Home team score)`
+// - Prompt the user to enter the score for the visiting team *(include the team name when prompting the user)*
+// `How many points did the GS Warriors score? (Visting team score)`
 
-// // In your program determine which team won the game with the highest score and display the results in an alert box. 
-// //Also program to show if the teams tied.
+// In your program determine which team won the game with the highest score and display the results in an alert box. 
+//Also program to show if the teams tied.
 
-// // Expected Output:
-// // ```
-// // Looks like the Memphis Grizzles have defeated the GS Warriors!
+// Expected Output:
+// ```
+// Looks like the Memphis Grizzles have defeated the GS Warriors!
 
-// // FINAL SCORE:
-// // Home/Memphis Grizzlies: 102
-// // Visitors/GS Warriors: 23
-// // ```
+// FINAL SCORE:
+// Home/Memphis Grizzlies: 102
+// Visitors/GS Warriors: 23
+// ```
 
 // //***Declaring variables */
 // let homeTeam = prompt("What is the name of your home team?");
