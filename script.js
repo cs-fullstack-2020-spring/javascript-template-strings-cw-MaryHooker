abb07c8cdf118fffa7f19f3085d7799cce09fc27
MaryHooker <mc020791@yahoo.com>
2020-02-18 14:55:31 -0600

    Added Comments

--- script.js
+++ script.js
@@ -20,17 +20,18 @@
 // // My id is 2112.
 // // My address is Memphis, TN.
 // // ```
+// //***Declaring variables */
 // let my_name = "Kevin"
 // let my_id = "2112"
 // let my_address = "Memphis, TN"
-
+// //***Creating template literal */
 // let my_template_string = 
 // `
 // My name is ${my_name}.
 // My id is ${my_id}.
 // My address is ${my_address}.
 // `
-
+// //***Printing literal in console */
 // console.log(my_template_string)
 
 // // ### Exercise 2:
@@ -46,60 +47,67 @@
 // // ```
 // // Congratulations 1_M_l337! !!, your score is 9001.
 // // ```
+// //****Declaring variables */
 // let contestant = "1_M_1337"
 // let score = "9001"
+// //***Creating template literal */
 // let new_string =
 // `
 // Congratulations ${contestant}!!!, your score is ${score}.
 // `
+// //***printing literal in console */
 // console.log(new_string)
 
-// ### Exercise 3:
+// // ### Exercise 3:
 
-// - Prompts the user for the name of their Home team
-// - Prompts the user for the name of the Visiting team
-// - Prompt the user to enter the score for their home team *(include the team name when prompting the user)*
-// `How many points did the Memphis Grizzlies score? (Home team score)`
-// - Prompt the user to enter the score for the visiting team *(include the team name when prompting the user)*
-// `How many points did the GS Warriors score? (Visting team score)`
+// // - Prompts the user for the name of their Home team
+// // - Prompts the user for the name of the Visiting team
+// // - Prompt the user to enter the score for their home team *(include the team name when prompting the user)*
+// // `How many points did the Memphis Grizzlies score? (Home team score)`
+// // - Prompt the user to enter the score for the visiting team *(include the team name when prompting the user)*
+// // `How many points did the GS Warriors score? (Visting team score)`
 
-// In your program determine which team won the game with the highest score and display the results in an alert box. 
-//Also program to show if the teams tied.
+// // In your program determine which team won the game with the highest score and display the results in an alert box. 
+// //Also program to show if the teams tied.
 
-// Expected Output:
-// ```
-// Looks like the Memphis Grizzles have defeated the GS Warriors!
+// // Expected Output:
+// // ```
+// // Looks like the Memphis Grizzles have defeated the GS Warriors!
 
-// FINAL SCORE:
-// Home/Memphis Grizzlies: 102
-// Visitors/GS Warriors: 23
-// ```
+// // FINAL SCORE:
+// // Home/Memphis Grizzlies: 102
+// // Visitors/GS Warriors: 23
+// // ```
 
-let homeTeam = prompt("What is the name of your home team?");
-let visitors = prompt("What is the name of the visiting team?");
-let homeScore = parseInt(prompt(`How many points did the ${homeTeam} score?`));
-let visitorScore = parseInt(prompt(`How many points did the ${visitors} score?`));
+// //***Declaring variables */
+// let homeTeam = prompt("What is the name of your home team?");
+// let visitors = prompt("What is the name of the visiting team?");
+// let homeScore = parseInt(prompt(`How many points did the ${homeTeam} score?`));
+// let visitorScore = parseInt(prompt(`How many points did the ${visitors} score?`));
 
-if(homeScore > visitorScore){
-    alert(`Looks like the ${homeTeam} have defeated the ${visitors}!
+// //***Conditionals */
+// if(homeScore > visitorScore){
+//     alert(`Looks like the ${homeTeam} have defeated the ${visitors}!
 
-    Final Score:
-    Home/${homeTeam}: ${homeScore}
-    Visitor/${visitors}: ${visitorScore}`)
-} else if(homeScore < visitorScore){
-    alert(`Looks like the ${visitors} have defeated the ${homeTeam}!
+//     Final Score:
+//     Home/${homeTeam}: ${homeScore}
+//     Visitor/${visitors}: ${visitorScore}`)
+// } else if(homeScore < visitorScore){
+//     alert(`Looks like the ${visitors} have defeated the ${homeTeam}!
 
-    Final Score:
-    Home/${homeTeam}: ${homeScore}
-    Visitor/${visitors}: ${visitorScore}`)
-} else if(homeScore==visitorScore){
-    alert(`Looks like the ${homeTeam} and the ${visitors} have tied!
+//     Final Score:
+//     Home/${homeTeam}: ${homeScore}
+//     Visitor/${visitors}: ${visitorScore}`)
+// } else if(homeScore==visitorScore){
+//     alert(`Looks like the ${homeTeam} and the ${visitors} have tied!
 
-    Final Score:
-    Home/${homeTeam}: ${homeScore}
-    Visitor/${visitors}: ${visitorScore} `)
-}
-console.log(`
-Final Score:
-Home/${homeTeam}: ${homeScore}
-Visitor/${visitors}: ${visitorScore}`)
+//     Final Score:
+//     Home/${homeTeam}: ${homeScore}
+//     Visitor/${visitors}: ${visitorScore} `)
+// }
+
+// //***Not in requirements but printing in console for myself */
+// console.log(`
+// Final Score:
+// Home/${homeTeam}: ${homeScore}
+// Visitor/${visitors}: ${visitorScore}`)
